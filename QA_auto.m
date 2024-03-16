3b8135d15a5eee637ecd2a97da81f7248321bac3
Tarun Naren <97557031+tnaren97@users.noreply.github.com>
2024-03-16 16:42:01 -0500

    major updates

--- QA_auto.m
+++ QA_auto.m
@@ -1,14 +1,14 @@
 clear all; close all; clc;
 
-options = {'kmeans','otsu','circle+contours','hough','hough+contours','edge+hough+contours'};
-[choice, tf] = listdlg('ListString', options, 'InitialValue', 5, 'PromptString', "Choose a segmentation method", 'SelectionMode', 'single');
-segType = options{choice};
-
-plane = inputdlg("Type in image plane name");
+plane = inputdlg("Type in analysis region name");
 plane = plane{1};
 
 num_roi = inputdlg("Type in number of ROIs in plane");
 num_roi = str2num(num_roi{1});
+
+list_options = {'kmeans','otsu','circle+contours','hough','hough+contours','edge+hough+contours'};
+[choice, tf] = listdlg('ListString', list_options, 'InitialValue', 5, 'PromptString', "Choose a segmentation method", 'SelectionMode', 'single');
+segType = list_options{choice};
 
 % plane = 'asc';
 % segType = 'hough+contours';
@@ -16,25 +16,34 @@ num_roi = str2num(num_roi{1});
 
 date = string(datetime('now', 'Format', 'yyyy-MM-dd-HHmm'));
 save_loc = uigetdir(pwd, "Choose where to save analysis data");
-% save_loc = 'D:\PWV\vol_Tarun';
-disp("Created analysis folder")
-cd(save_loc)
-result_dir = fullfile(save_loc, '2DPC_QA_Analysis');
-if ~exist(result_dir,'dir')
-    mkdir(result_dir);
+
+[~, save_name] = fileparts(save_loc);
+if strcmp(save_name, '2DPC_QA_Analysis')
+    result_dir = save_loc;
+else
+    result_dir = fullfile(save_loc, '2DPC_QA_Analysis');
+    if ~exist(result_dir,'dir')
+        mkdir(result_dir);
+    end
 end
+% save_loc = 'D:\PWV\vol_Tarun';
 analysis_dir = fullfile(result_dir, plane, strcat(segType, '_', date));
+if ~exist(analysis_dir,'dir')
+    mkdir(analysis_dir)
+end
+disp("Created analysis folder")
 
 %% Load BSSFP
-bssfp_folder = uigetdir(pwd, 'Select bSSFP Scan');
-% bssfp_folder = 'D:\PWV\vol_Tarun\standard\015_FIESTA_Cine_BH_AAo_NOPROSP_14VPS_30FRAMES';
-disp("Loading bSSFP data...")
+
 bssfp_data_folder = fullfile(result_dir, plane, 'bssfp');
 if ~exist(bssfp_data_folder,'dir')
     mkdir(bssfp_data_folder);
 end
-
+disp("Loading bSSFP data...")
 if ~exist(fullfile(bssfp_data_folder, 'bssfp.mat'),'file')
+    disp("Select bSSFP scan folder")
+    bssfp_folder = uigetdir(pwd, 'Select bSSFP scan folder');
+    % bssfp_folder = 'D:\PWV\vol_Tarun\standard\015_FIESTA_Cine_BH_AAo_NOPROSP_14VPS_30FRAMES';  
     dirFiles = dir(fullfile(bssfp_folder, '*.dcm'));
     if isempty(dirFiles) %check if dicoms are zipped
         zipFiles = dir(fullfile(bssfp_folder, '*.tgz'));
@@ -49,8 +58,6 @@ if ~exist(fullfile(bssfp_data_folder, 'bssfp.mat'),'file')
         end 
     end     
     bssfpInfo = dicominfo(fullfile(dirFiles(1).folder, dirFiles(1).name)); %grab dicom info from 1st frame
-    bssfpSize = double(bssfpInfo.Height); %assumes matrix size is equal in x and y
-    bssfpFrames = bssfpInfo.CardiacNumberOfImages;
     bssfp = zeros(bssfpSize,bssfpSize,bssfpFrames);
     for f=1:length(dirFiles)
         bssfp(:,:,f) = dicomread(fullfile(dirFiles(f).folder, dirFiles(f).name));
@@ -67,14 +74,10 @@ else
 end 
 bssfpSize = double(bssfpInfo.Height); %assumes matrix size is equal in x and y
 bssfpFrames = bssfpInfo.CardiacNumberOfImages;
-pixelArea = bssfpInfo.PixelSpacing(1).*bssfpInfo.PixelSpacing(2)*0.01; %cm^2
-
+pixelArea = bssfpInfo.PixelSpacing(1).*bssfpInfo.PixelSpacing(2)*1e-06; % m^2
 tavg_bssfp = mean(bssfp, 3);
 
 %% Initialize Masks
-if ~exist(analysis_dir,'dir')
-    mkdir(analysis_dir)
-end
 answer = questdlg("Load masks?");
 switch answer
     case 'Yes'
@@ -101,6 +104,7 @@ switch answer
         return
 end 
 
+disp("Segmenting bSSFP images")
 radrange = [10 30]; % radius range for Hough Transform
 sens = 0.8; % sensitivity of circle find
 %% Segment Each Frame Separately
@@ -212,7 +216,7 @@ if needsMask % if we haven't loaded in the mask...
             vessel = padarray(vessel, [crop(1) crop(3)], 'pre');
             vessel = padarray(vessel, [bssfpSize-crop(2)-1 bssfpSize-crop(4)-1], 'post');
             mask(j,:,:,i) = vessel;
-            area(j, i) = sum(vessel(:))*pixelArea; %cm^2
+            area(j, i) = sum(vessel(:))*pixelArea; %m^2
         end
         close all
     end
@@ -222,14 +226,16 @@ if needsMask % if we haven't loaded in the mask...
 end
 
 %% Load 2DPC
-pc_folder = uigetdir(pwd,'Select folder 2DPC scan');
-% pc_folder = 'D:\PWV\vol_Tarun\standard\008_PWV_CartBH_AAo_NOPROSP_4VPS_40FRAMES';
-disp("Loading 2DPC data...")
+
 pc_data_folder = fullfile(result_dir, plane, 'pc');
 if ~exist(pc_data_folder,'dir')
     mkdir(pc_data_folder);
 end
+disp("Loading 2DPC data...")
 if ~exist(fullfile(pc_data_folder, 'pc.mat'),'file')
+    disp("Select 2DPC scan folder")
+    pc_folder = uigetdir(pwd,'Select 2DPC scan folder');
+    % pc_folder = 'D:\PWV\vol_Tarun\standard\008_PWV_CartBH_AAo_NOPROSP_4VPS_40FRAMES';
     dirFiles = dir(fullfile(pc_folder, '*.dcm'));
     if isempty(dirFiles) %check if dicoms are zipped
         zipFiles = dir(fullfile(pc_folder, '*.tgz'));
@@ -245,8 +251,6 @@ if ~exist(fullfile(pc_data_folder, 'pc.mat'),'file')
     end 
     
     pcInfo = dicominfo(fullfile(dirFiles(1).folder, dirFiles(1).name)); %grab dicom info from 1st frame
-    pcSize = double(pcInfo.Height); %assumes matrix size is equal in x and y
-    pcFrames = pcInfo.CardiacNumberOfImages;
     pcmr = zeros(pcSize,pcSize,pcFrames);
     for f=1:length(dirFiles)
         pcmr(:,:,f) = dicomread(fullfile(dirFiles(f).folder, dirFiles(f).name));
@@ -255,6 +259,10 @@ if ~exist(fullfile(pc_data_folder, 'pc.mat'),'file')
     end
     pc = pcmr(:,:,1:pcFrames);
     mag = pcmr(:,:,(pcFrames+1):end);
+    f = figure; imshow3D(mag); 
+    % movegui(f, 'west');
+    % g = figure; imshow3D(pc)
+    % movegui(g, 'east');
     pcmrTimes = pcmrTimes(1:pcFrames);
     save(fullfile(pc_data_folder, 'pc.mat'), 'pc');
     save(fullfile(pc_data_folder, 'mag.mat'), 'mag');
@@ -268,6 +276,22 @@ else
 end 
 pcSize = double(pcInfo.Height); %assumes matrix size is equal in x and y
 pcFrames = pcInfo.CardiacNumberOfImages;
+
+
+%% Save ROI images
+% f = figure; imoverlay(rescale(tavg_bssfp), squeeze(sum(sum(mask, 1), 4)));
+% frame_bssfp = getframe(f);
+% imwrite(frame2im(frame_bssfp),fullfile(analysis_dir, 'tavg_bssfp_mask.png'));
+% close(f);
+% g = figure; imoverlay(mean(mag, 3), squeeze(sum(sum(mask, 1), 4)));
+% frame_mag = getframe(g);
+% imwrite(frame2im(frame_mag),fullfile(analysis_dir, 'mag_mask.png'));
+% close(g);
+% temp_mask = squeeze(sum(mask, 1));
+% h = figure; imoverlay(pc(:,:, 1), temp_mask(:,:,1));
+% frame_mag = getframe(h);
+% imwrite(frame2im(frame_mag),fullfile(analysis_dir, 'pc_mask.png'));
+% close(h);
 
 
 %% Make temporal resolutions equivalent
@@ -290,28 +314,51 @@ pcmrTimes = interp1((1:pcFrames),pcmrTimes,linspace(1,pcFrames,desiredFrames),'l
 bssfpTimes = interp1((1:bssfpFrames),bssfpTimes,linspace(1,bssfpFrames,desiredFrames),'linear');
 
 
-%% Shift the data due to periph. gating lag + prosp. gating lags
-%bssfp = circshift(bssfp,15,3);
-%pc = circshift(pc,15,3);
-%mag = circshift(mag,15,3);
-
-
 %% Calculating Velocity and Flow
 for j=1:num_roi
     for i=1:desiredFrames
         ROI = imbinarize(mask(j,:,:,i));
-        vTemp = pc(:,:,i)*0.1; %single frame velocities (cm/s)
+        vTemp = pc(:,:,i)*0.001; %single frame velocities (m/s)
         ROIindex = double(vTemp(ROI)); %indexed velocities within mask
-        meanV = mean(ROIindex); %mean velocity in frame i (cm/s)
-        flow(j, i) = area(j, i).*meanV; %flow in frame i (cm^3/s)
+        meanV = mean(ROIindex); %mean velocity in frame i (m/s)
+        flow(j, i) = area(j, i).*meanV; %flow in frame i (m^3/s)
     end
 end
-stroke_volume = sum(flow, 2);
 save(fullfile(analysis_dir, 'flows.mat'), 'flow');
-disp('Flow Data Saved')
+disp('Flow data saved')
 
+%% Shift the data due to periph. gating lag + prosp. gating lags
+flag = 0;
+shift = 0;
+options.WindowStyle = 'normal';
+while ~flag
+    f = figure;
+    if mean(flow(1,:))<0
+        plot(circshift(-1*flow(1, :), shift)); 
+    else
+        plot(circshift(flow(1, :), shift));
+    end
+    % movegui(f, 'west');
+    answer = inputdlg({"Enter shift amount:", "Accept? (enter 1)"}, "Flow curve shift", ...
+        [1 30; 1 30], {num2str(shift), num2str(flag)}, options);
+    shift = str2num(answer{1});
+    flag = str2num(answer{2});
+    close(f)
+end
+bssfp = circshift(bssfp,shift,3);
+pc = circshift(pc,shift,3);
+mag = circshift(mag,shift,3);
+for i=1:num_roi
+    mask(i, :,:,:) = circshift(mask(i, :,:,:), shift, 4);
+    area(i, :) = circshift(area(i, :), shift, 2);
+    flow(i, :) = circshift(flow(i, :), shift, 2);
+end
+mean_rr = mean([pcInfo.LowRRValue, pcInfo.HighRRValue])*0.001; % mean rr interval in s
+stroke_volume = abs(sum(flow, 2)./(60/mean_rr));
+
+
+%% Compute PWV
 for j=1:num_roi
-    %% Compute PWV
     if mean(flow(j,:))<0
         flow(j, :) = -1*flow(j, :);
     end      
@@ -320,7 +367,7 @@ for j=1:num_roi
     
     %Define Systolic Region (flow vs. time)
     figure; plot(flow(j, :)); 
-    title(sprintf('%s: ROI %d - Flow', plane, j)); xlabel('Time Frame'); ylabel('Flow (cm^3/s)');
+    title(sprintf('%s: ROI %d - Flow', plane, j)); xlabel('Time Frame'); ylabel('Flow (m^3/s)');
     disp("Draw around the upslope portion of the curve")
     flow_plot = gcf;
     free = drawfreehand;
@@ -333,10 +380,8 @@ for j=1:num_roi
     x = area_int(systoleTimes); %time adjustment (fixing delays)
     y = flow(j, systolePts);
     figure; scatter(x,y); 
-    title(sprintf('%s: ROI %d - QA Plot', plane, j)); xlabel('Area (cm^2)'); ylabel('Flow (cm^3/s)');
-    free2 = drawfreehand;
-    in = inpolygon(x,y,free2.Position(:,1),free2.Position(:,2));
-    systolePts2 = [x(in); y(in)]';
+    title(sprintf('%s: ROI %d - QA Plot', plane, j)); xlabel('Area (m^2)'); ylabel('Flow (m^3/s)');
+    systolePts2 = [x; y]';
     [coef,stats] = polyfit(systolePts2(:,1),systolePts2(:,2),1);
     
     %Linear regression
@@ -344,23 +389,21 @@ for j=1:num_roi
     maxArea = max(systolePts2(:,1));
     xq = linspace(minArea-0.1,maxArea+0.1,100);
     yq = coef(1)*xq + coef(2); %y = mx+b
-    delete(free2)
     hold on; 
     scatter(systolePts2(:,1),systolePts2(:,2),72,'k','x');
     plot(xq,yq); 
-    str = ['Slope = ' num2str(coef(1)) '\newlineIntcpt = ' num2str(coef(2))];
-    text(min(xq),max(yq)-0.1*max(yq),str);
+    str = [];
+    text(min(xq),max(yq)-0.1*max(yq),sprintf('Slope = %.3f m/s \n Intcpt = %.3f', coef(1), coef(2)));
     hold off;
     QAplot = gcf; 
     DescAo_PWV = coef(1);
-    fprintf('ROI %d PWV_QA = %.4f m/s\n', j, coef(1)*0.01);
-    fprintf('ROI %d Stroke Volume = %.4f mL\n', j, stroke_volume(j)*0.1)
+    fprintf('ROI %d PWV_QA = %.4f m/s\n', j, coef(1));
+    fprintf('ROI %d Stroke Volume = %.4f L\n', j, stroke_volume(j));
     saveas(QAplot,fullfile(analysis_dir, sprintf('QAplot_ROI_%d', j)));
     save(fullfile(analysis_dir, sprintf('coef_ROI_%d.mat', j)), 'coef');
     save(fullfile(analysis_dir, sprintf('stats_ROI_%d.mat', j)), 'stats');
 end
-disp('PWV-QA Data Saved!')
-disp("(•_•)    ( •_•)>⌐■-■     (⌐■_■)")
+disp("( つ ◕_◕ )つ PWV-QA Data Saved! ( つ ◕_◕ )つ")
 
 
 %% Helper functions
@@ -368,9 +411,11 @@ function [BW, centers, radii, radrange, sens] = circleFinder(image, num_roi, rad
     BW = false(size(image,1),size(image,2));
     [Xgrid,Ygrid] = meshgrid(1:size(BW,2),1:size(BW,1));
     flag = 0;
+    mflag = 0;
     while ~flag
         g = figure;
         imshow(image, []);
+        movegui(g, 'onscreen');
         [centers,radii,~] = imfindcircles(image,radrange,'ObjectPolarity','bright','Sensitivity',sens);
         for n = 1:num_roi
             try
@@ -381,17 +426,32 @@ function [BW, centers, radii, radrange, sens] = circleFinder(image, num_roi, rad
         end
         viscircles(centers, radii);
         options.WindowStyle = 'normal';
-        answer = inputdlg({"Enter lower radius range:", "Enter upper radius range:", "Enter sensitivity:", "Accept? (enter 1)"}, ...
-            "Circle Estimation Parameters", [1 30; 1 30; 1 30; 1 30;], {num2str(radrange(1)), num2str(radrange(2)), ...
-            num2str(sens), num2str(flag)}, options);
+        answer = inputdlg({"Enter lower radius range:", "Enter upper radius range:", "Enter sensitivity:", "Draw manually? (enter 1)", "Accept? (enter 1)"}, ...
+            "Circle Estimation Parameters", [1 30; 1 30; 1 30; 1 30; 1 30;], {num2str(radrange(1)), num2str(radrange(2)), ...
+            num2str(sens), num2str(mflag), num2str(flag)}, options);
         close(g)
         radrange = [str2num(answer{1}) str2num(answer{2})];
         sens = str2double(answer{3});
-        flag = str2num(answer{4});
+        mflag = str2num(answer{4});
+        flag = str2num(answer{5});
+        if mflag
+            centers = zeros(num_roi, 2);
+            radii = zeros(num_roi, 1);
+            for z=1:num_roi
+                k = figure; 
+                imshow(image, []);
+                circle = drawcircle();
+                radii(z) = circle.Radius; %get radius of circle
+                centers(z,:) = round(circle.Center); %get center coordinates
+                close(k);
+                BW = BW | (hypot(Xgrid-centers(n,1),Ygrid-centers(n,2)) <= radii(n));
+            end
+            flag = 1;
+        end
         if flag
             if size(centers, 1) ~= num_roi
                 fprintf("Error: Number of ROIs is greater than or less than %d.\n" + ...
-                    "Please continue adjusting parameters", num_roi);
+                    "Please continue adjusting parameters\n", num_roi);
                 flag = 0;
             end
         end
